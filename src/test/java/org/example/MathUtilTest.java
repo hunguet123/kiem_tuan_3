8f53657d65ae27ba85ed7cb68390b4134d252155
hunghq <hunghq@yopaz.vn>
2023-09-25 21:55:28 +0700

    finish

--- src/test/java/org/example/MathUtilTest.java
+++ src/test/java/org/example/MathUtilTest.java
@@ -4,9 +4,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class MathUtilTest {
     MathUtil util = new MathUtil();
-
-
-    // test với bảng quyết định test1 -> test16
+    
+    // test với bảng quyết định test1 -> test5
     @Test
     void test1(){
         assertEquals(util.calculateInterest(1000.0, 0.05, 1, 1), 1050.00);
@@ -14,7 +13,7 @@ public class MathUtilTest {
 
     @Test
     void test2(){
-        assertEquals(util.calculateInterest(1000.0, 0.05, 1, -12), 0);
+        assertEquals(util.calculateInterest(1000.0, -0.05, 1, -12), 0);
     }
 
     @Test
@@ -29,88 +28,32 @@ public class MathUtilTest {
 
     @Test
     void test5(){
-        assertEquals(util.calculateInterest(5000.0, -0.03, 1, 1), 0);
+        assertEquals(util.calculateInterest(-5000.0, 0.03, 1, 1), 0);
     }
+    // test với phân hoạch tương đương test 6 -> test10
 
     @Test
     void test6(){
-        assertEquals(util.calculateInterest(5000.0, -0.03, 1, -12), 0);
-    }
-
-    @Test
-    void test7(){
-        assertEquals(util.calculateInterest(5000.0, -0.03, -5, 1), 0);
-    }
-
-    @Test
-    void test8(){
-        assertEquals(util.calculateInterest(5000.0, -0.03, -5, -12), 0);
-    }
-
-    @Test
-    void test9(){
-        assertEquals(util.calculateInterest(-2000.0, 0.08, 1, 1), 0);
-    }
-
-    @Test
-    void test10(){
-        assertEquals(util.calculateInterest(-2000.0, 0.08, 1, -12), 0);
-    }
-
-    @Test
-    void test11(){
-        assertEquals(util.calculateInterest(-2000.0, 0.08, -5, 1), 0);
-    }
-
-    @Test
-    void test12(){
-        assertEquals(util.calculateInterest(-2000.0, 0.08, -5, -12), 0);
-    }
-
-    @Test
-    void test13(){
-        assertEquals(util.calculateInterest(-10000.0, -0.06, 1, 1), 0);
-    }
-
-    @Test
-    void test14(){
-        assertEquals(util.calculateInterest(-10000.0, -0.06, 1, -12), 0);
-    }
-
-    @Test
-    void test15(){
-        assertEquals(util.calculateInterest(-10000.0, -0.06, -5, 1), 0);
-    }
-
-    @Test
-    void test16(){
-        assertEquals(util.calculateInterest(-10000.0, -0.06, -5, -12), 0);
-    }
-
-    // test với phân hoạch tương đương test 17 -> test21
-
-    @Test
-    void test17(){
         assertEquals(util.calculateInterest(1000.0, 0.05, 5, 1), 1276.2815625000003);
     }
 
     @Test
-    void test18(){
+    void test7(){
         assertEquals(util.calculateInterest(1000.0, -2, 5, 4), 0);
     }
 
     @Test
-    void test19(){
+    void test8(){
         assertEquals(util.calculateInterest(1000.0, -2, -4, 7), 0);
     }
 
     @Test
-    void test20(){
+    void test9(){
         assertEquals(util.calculateInterest(1000.0, -2, -4, -5), 0);
     }
 
     @Test
-    void test21(){
+    void test10(){
         assertEquals(util.calculateInterest(-1333.0, -3, -4, -5), 0);
     }
 }
