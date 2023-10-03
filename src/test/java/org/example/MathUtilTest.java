ece439f84084622006295f38610e9ef643624e75
hunghq <hunghq@yopaz.vn>
2023-10-03 23:54:28 +0700

    week4

--- src/test/java/org/example/MathUtilTest.java
+++ src/test/java/org/example/MathUtilTest.java
@@ -8,52 +8,11 @@ public class MathUtilTest {
     // test với bảng quyết định test1 -> test5
     @Test
     void test1(){
-        assertEquals(util.calculateInterest(1000.0, 0.05, 1, 1), 1050.00);
+        assertEquals(util.calculateInterest(-5, 1, 1, 1), 0);
     }
 
     @Test
     void test2(){
-        assertEquals(util.calculateInterest(1000.0, -0.05, 1, -12), 0);
-    }
-
-    @Test
-    void test3(){
-        assertEquals(util.calculateInterest(1000.0, 0.05, -5, 1), 0);
-    }
-
-    @Test
-    void test4(){
-        assertEquals(util.calculateInterest(1000.0, 0.05, -5, -12), 0);
-    }
-
-    @Test
-    void test5(){
-        assertEquals(util.calculateInterest(-5000.0, 0.03, 1, 1), 0);
-    }
-    // test với phân hoạch tương đương test 6 -> test10
-
-    @Test
-    void test6(){
-        assertEquals(util.calculateInterest(1000.0, 0.05, 5, 1), 1276.2815625000003);
-    }
-
-    @Test
-    void test7(){
-        assertEquals(util.calculateInterest(1000.0, -2, 5, 4), 0);
-    }
-
-    @Test
-    void test8(){
-        assertEquals(util.calculateInterest(1000.0, -2, -4, 7), 0);
-    }
-
-    @Test
-    void test9(){
-        assertEquals(util.calculateInterest(1000.0, -2, -4, -5), 0);
-    }
-
-    @Test
-    void test10(){
-        assertEquals(util.calculateInterest(-1333.0, -3, -4, -5), 0);
+        assertEquals(util.calculateInterest(1, 1, 1, 1), 2);
     }
 }
